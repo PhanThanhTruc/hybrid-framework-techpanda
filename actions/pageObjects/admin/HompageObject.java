e8be3959398a2c35b8033a569db2e995102de0ce
PhanThanhTruc <phanthithanhtruc98@gmail.com>
2022-12-21 23:09:25 +0700

    Restructure project

---
+++ actions/pageObjects/admin/HompageObject.java
@@ -0,0 +1,6 @@
+package pageObjects.admin;
+
+public class HompageObject {
+	//hàm để tương tác giữa các element
+
+}
---
+++ interfaces/pageUIs/admin/HomePageUI.java
@@ -0,0 +1,7 @@
+package pageUIs.admin;
+
+public class HomePageUI {
+	// define các locator(UI) của element thuộc page đó
+	public static String REGISTER_BUTTON = "";
+
+}
